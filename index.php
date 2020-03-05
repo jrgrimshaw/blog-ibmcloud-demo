999d7961610f9c068fd70e62e9750e8a1b7e37b8
James R Grimshaw <James.Grimshaw@ibm.com>
2020-03-05 13:20:39 +0000

    First commit

---
+++ index.php
@@ -0,0 +1,30 @@
+<html>
+
+<head>
+  <title>My Blog</title>
+  <link rel="stylesheet" href="style.css">
+</head>
+
+<body>
+  <div class="container">
+    <h1>Marina's Shop</h1>
+
+    <nav>
+      <a href="">Home</a> | <a href="">Products</a> | <a href="">Cart</a>
+    </nav>
+
+    <div class="posts">
+      <?php for ($i = 0; $i < 5; $i++) : ?>
+        <div class="post">
+          <img src="tea-image.jpg" alt="">
+          <p><strong>Green Tea Set</strong></p>
+          <p><em>£49.99</em></p>
+          <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Vero, libero odio. Ex officia dicta id quo eaque aut harum cumque optio iste itaque nemo maiores similique, eos debitis exercitationem consectetur modi amet, eveniet provident corporis neque repellat dolorum laudantium?</p>
+        </div>
+      <?php endfor; ?>
+    </div>
+  </div>
+
+</body>
+
+</html>
